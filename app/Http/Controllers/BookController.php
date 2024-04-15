70e549236f80a70511197c76c817b28436c1d621
Kirsten <candykaye87@gmail.com>
2024-04-15 11:16:57 +0800

    controller sample addition comment

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -13,4 +13,18 @@ class BookController extends Controller
         $books = Book::all();
         return view('books.index', compact('books'));
     }
+
+    public function createBooks()
+    {
+        for ($i = 0; $i < 20; $i++) {
+            $book = new Book();
+            $book->isbn = 'ISBN' . ($i + 1); // Example ISBN format
+            $book->title = 'Book Title ' . ($i + 1);
+            $book->author = 'Author Name';
+            $book->description = 'Description of the book';
+            $book->date_published = now(); // Example date
+            $book->save();
+        }
+    }
+    
 }
